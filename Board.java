74e0823e84db4a0a595ad998614e4dae00c51631
Tyler Glenn <glennt3@quest-OptiPlex-9020>
2016-02-15 19:56:10 -0800

    Add board class

---
+++ Board.java
@@ -0,0 +1,31 @@
+public class Board{
+   
+   private Player[] players;    
+   private int numdays;
+   private int cards=10;
+   
+   
+   
+   public Board( int numdays,Player[] players){
+      
+      this.numdays=numdays;
+      this.players=players;
+      
+   
+   }
+   
+   public static void createRooms(){
+      Room Trailer = new Room(
+   }
+   
+   public static void startGame(){
+      while(numdays>0){
+         while(cards>1){
+         
+         }
+         numdays-=1;
+         
+      }
+   }
+
+}
